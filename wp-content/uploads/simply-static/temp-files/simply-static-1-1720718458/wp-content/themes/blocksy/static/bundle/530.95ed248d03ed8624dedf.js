758e6cd334e28f99f02ef6f42e4152eb0220f8bb
Sarvesh Kesharwani <sarveshkumar5513@gmail.com>
2024-07-13 23:33:32 +0530

    updated pag content and added a static home page

--- wp-content/uploads/simply-static/temp-files/simply-static-1-1720718458/wp-content/themes/blocksy/static/bundle/530.95ed248d03ed8624dedf.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.blocksyJsonP=self.blocksyJsonP||[]).push([[530],{3530:function(n,s,t){t.r(s),t.d(s,{mountGreenshiftIntegration:function(){return o}});const o=function(){window.gsInitTabs&&window.gsInitTabs(".gspb-tabs")}}}]);
